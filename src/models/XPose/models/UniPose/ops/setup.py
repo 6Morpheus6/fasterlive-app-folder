090372b075285c5a5e1cb312090f50534870f9b9
6Morpheus6 <wanton-decay@gmx.net>
2026-06-02 03:47:28 +0200

    rtx 50 / torch+cu128 support

--- src/models/XPose/models/UniPose/ops/setup.py
+++ src/models/XPose/models/UniPose/ops/setup.py
@@ -53,7 +53,8 @@ def get_extensions():
             "-gencode=arch=compute_80,code=sm_80",
             "-gencode=arch=compute_86,code=sm_86",
             "-gencode=arch=compute_89,code=sm_89",
-            "-gencode=arch=compute_90,code=sm_90"
+            "-gencode=arch=compute_90,code=sm_90",
+            "-gencode=arch=compute_120,code=sm_120",
         ]
     else:
         raise NotImplementedError('Cuda is not availabel')
